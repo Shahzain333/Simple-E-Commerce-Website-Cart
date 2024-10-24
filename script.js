3f9a4d8dc9ea132d3795b055afba3acfa5410670
Shahzain khan <108424969+Shahzain333@users.noreply.github.com>
2024-10-24 17:36:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,8 @@ document.addEventListener("DOMContentLoaded", () => {
         { id: 3, name: "Product 3", price: 49.99 },
     ];
     
-    const cart = []
+    let cart = []
     
-
     products.forEach(product => {
 
         const productDiv = document.createElement('div');
@@ -57,23 +56,41 @@ document.addEventListener("DOMContentLoaded", () => {
             emptyCartMessage.classList.add('hidden');
             cartTotalMessage.classList.remove('hidden');
 
-            cart.forEach( (item) => {
+            cart.forEach(item => {
 
                 totalPrice += item.price;
 
                 const cartItem = document.createElement('div');
                 cartItem.innerHTML = `
-                ${item.name} - ${item.price.toFixed(2)}`
+                ${item.name} - ${item.price.toFixed(2)}
+                <button data-id="${item.id}">Remove</button>`
+                
+                cartItem.style.padding = '2px';
+                cartItem.style.display = 'flex';
+                cartItem.style.alignItems = 'center';
+                cartItem.style.justifyContent = 'space-between';
 
                 cartItems.appendChild(cartItem);
                 totalPriceDisplay.textContent = `${totalPrice.toFixed(2)}`
-            });
 
+            });
         }else {
             emptyCartMessage.classList.remove('hidden');
             cartTotalMessage.classList.add('hidden');
         }
         
+    }
+
+    cartItems.addEventListener('click', (e) => {
+        if(e.target.tagName === "BUTTON"){
+            const removeProductId = parseInt(e.target.getAttribute('data-id'));
+            removeFromCart(removeProductId); 
+        }
+    });
+
+    function removeFromCart(productId) {
+        cart = cart.filter((product) => product.id !== productId); // Remove the product
+        renderCart(); // Re-render the cart
     }
 
     checkoutBtn.addEventListener('click', ()=> {
